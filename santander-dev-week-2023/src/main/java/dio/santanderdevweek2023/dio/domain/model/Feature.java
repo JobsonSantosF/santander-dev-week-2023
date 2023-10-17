becbd141422a2c59d6d3c51796e924a019f7ca68
JobsonSantosF <jobson.filho@hotmail.com>
2023-10-17 03:39:06 -0300

    Implementação da camada de dominio

---
+++ santander-dev-week-2023/src/main/java/dio/santanderdevweek2023/dio/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package dio.santanderdevweek2023.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ santander-dev-week-2023/src/main/java/dio/santanderdevweek2023/dio/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dio.santanderdevweek2023.dio.domain.repository;
+
+import dio.santanderdevweek2023.dio.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
